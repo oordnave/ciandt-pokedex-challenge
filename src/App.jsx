a2c09f45175cb11378e635cfc52b0e722bdd6df4
Evandro Nascimento <oordnave@gmail.com>
2023-09-25 19:17:36 -0300

    add components folder, changes App.jsx file with some base markup

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,33 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <header>
+        <div className="buttons">
+          <div className="favorites">
+            <a href="">Favorites</a>
+          </div>
+          <div className="compare">
+            <a href="">Compare</a>
+          </div>
+        </div>
+        <div className="logo">
+          <img src="" alt="" />
+          <p>Pokédex</p>
+        </div>
+      </header>
+      <section>
+        <div className="search">
+          <form action="">
+            <input type="text" text="" />
+          </form>
+        </div>
+        <div className="list-all-button">
+          <button>
+            List all Pokémons
+          </button>
+        </div>
+      </section>
+      <footer></footer>
    </>
   )
 }
