49b7adefd442d3f95a73e2cbd94b2847bfd9005d
Evandro Nascimento <oordnave@gmail.com>
2023-09-25 23:18:49 -0300

    changes App.jsx file, basic components and requests, deletes react.svg file, adds css components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,77 @@
+// make the imports
+import { useState, useEffect } from 'react'
+
+// declaring constants
+const POKE_URL = 'https://pokeapi.co/api/v2'
+
+// App component
 function App() {
+  // states
+  // states for all the pokémons
+  const [pokemons, setPokemons] = useState([])
+
+  // state for the search query and search result
+  const [search, setSearch] = useState('')
+
+  const [searchResult, setSearchResult] = useState([])
+
+  // handlers
+  // test handler to reference
+  const testClick = () => console.log('working');
+
+  // handler to the search input
+  const handleSearch = (event) => {
+    setSearch(event.target.value)
+  }
+
+  // handler to perform a search
+  const searchPokemon = (event) => {
+    // preventing the default behaviour from the form
+    event.preventDefault();
+
+    // first, verify in the pokemons state if the pokemon exists
+    let result = pokemons.find(pokemon => pokemon.name === search)
+
+    if(result) {
+      console.log(result)
+    } else {
+      console.log('pokémon not found');
+    }
+    // let result = pokemons.find(pokemon => pokemon.name === search)
+    // console.log(result)
+
+
+
+    // verify if the search exists in the current pokemon state
+    // pokemons.find(pokemon => {
+    //   pokemon.name === event.
+    // })
+  }
+
+  // make the request for the API
+  const getPokemons = () => {
+    async function fetchData() {
+      try {
+        // make the request for the api, with limit of 20
+        const response = await fetch(POKE_URL + "/pokemon/?limit=20")
+        // convert the data to a json type
+        const data = await response.json()
+        // change the state  
+        setPokemons(data.results)
+      } catch (err) {
+        // error handling
+         console.log(err)
+      }
+    }
+
+    // execute the function
+    fetchData()
+  }
+
+  // effects
+  // using to sync with external systems - such as API calls
+  useEffect(getPokemons, [])
+
   return (
     <>
       <header>
@@ -7,7 +80,7 @@ function App() {
             <a href="">Favorites</a>
           </div>
           <div className="compare">
-            <a href="">Compare</a>
+            <a href="" className="text-3xl">Compare</a>
           </div>
         </div>
         <div className="logo">
@@ -17,18 +90,18 @@ function App() {
       </header>
       <section>
         <div className="search">
-          <form action="">
-            <input type="text" text="" />
+          <form action="" onSubmit={searchPokemon}>
+            <input type="text" value={search} onChange={handleSearch} />
           </form>
         </div>
-        <div className="list-all-button">
+        <div className="list-all-button" onClick={testClick}>
           <button>
             List all Pokémons
           </button>
         </div>
         <div className="list-pokemon">
           <ul>
-            <li></li>
+            {pokemons.map((pokemon, index) => <li key={index}>{index} {pokemon.name}</li>)}
           </ul>
         </div>
       </section>
