39d89b84b71ff9ffd9f3c681332f9650415093ba
Evandro Nascimento <oordnave@gmail.com>
2023-09-26 17:49:40 -0300

    changes App.jsx file, adds try catch and changes output

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from 'react'
 
 // declaring constants
-const POKE_URL = 'https://pokeapi.co/api/v2/pokemon/'
+const POKE_URL = 'https://pokeapi.co/apis/v2/pokemon/'
 
 // App component
 function App() {
@@ -50,34 +50,41 @@ function App() {
   const getPokemonData = async (length) => {
     // defining an empty array to store the promises
     const promiseArray = [];
-    
-    try {
-      // populate the array with promises
-      // this time, we make the requests for the api based on the id from the pokemon
-      for (let i = length; i < length + 20; i++) {
-        // pushing the data to update the array for each offset
-        promiseArray.push(
-          (await fetch(POKE_URL + `${i}`)).json()
-        )
 
+    // populate the array with promises
+    // this time, we make the requests for the api based on the id from the pokemon
+    for (let i = length; i < length + 20; i++) {
+      // variable to store the promise
+      
+      try {
+        // separate in variables the steps, to better readability 
+        let promise = (await fetch(POKE_URL + `${i}`)).json()
+
+        // push into the state array
+        promiseArray.push(promise)
+
+      // error caching
+      } catch(error) {
+        console.log(error.message)
+        setMessage(error.message)
+        break
       }
 
-      // wait for the promise array to resolve
-      const allPokemonData = await Promise.all(promiseArray);
-
-      // return the data from each pokemon 
-      return allPokemonData.map(pokemon => {
-        return {
-          name: pokemon.name,
-          sprite: pokemon.sprites.front_default,
-          // accessing the property with hiphen
-          artwork: pokemon.sprites.other['official-artwork'].front_default,
-          stats: pokemon.stats
-        }
-      })
-    } catch (err) {
-      console.log(err)
     }
+
+    // wait for the promise array to resolve
+    const allPokemonData = await Promise.all(promiseArray);
+
+    // return the data from each pokemon 
+    return allPokemonData.map(pokemon => {
+      return {
+        name: pokemon.name,
+        sprite: pokemon.sprites.front_default,
+        // accessing the property with hiphen
+        artwork: pokemon.sprites.other['official-artwork'].front_default,
+        stats: pokemon.stats
+      }
+    })
 
   }
 
@@ -166,7 +173,7 @@ function App() {
                   'offsetHeight: ', document.documentElement.offsetHeight)}
               </li>
             ))}
-            {isLoading && <h1 className="pokemonName">{message}</h1>}
+            {isLoading ? <h1 className="pokemonName">{ message }</h1> : <h1 className="pokemonName">{message}</h1>}
           </ul>
         </div>
       </section>
