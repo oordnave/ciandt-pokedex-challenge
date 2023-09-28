1efeccbe2df47d0ffaed9ffe33548fbf018f6449
Evandro Nascimento <oordnave@gmail.com>
2023-09-28 03:23:17 -0300

    changes files, modifies search component

--- src/components/blocks/Search/Search.jsx
+++ src/components/blocks/Search/Search.jsx
@@ -19,7 +19,7 @@ const Search = ({ searchPokemon, value, handleSearch, handleClear }) => {
           >
             <svg
               xmlns='http://www.w3.org/2000/svg'
-              class='h-5 w-5 mr-2'
+              className='h-5 w-5 mr-2'
               fill='none'
               viewBox='0 0 24 24'
               stroke='currentColor'
