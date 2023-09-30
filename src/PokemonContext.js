2c2fdfced254bf9146453cbeaccfdf15213f246c
Evandro Nascimento <oordnave@gmail.com>
2023-09-30 16:12:00 -0300

    changes files, modifies PokemonContext, adds prop types validation

--- src/PokemonContext.js
+++ src/PokemonContext.js
@@ -3,14 +3,17 @@
 // from the app in a way to be accessed while navigating and passing
 // the state for the routes
 
+// importing prop types
+import PropTypes from 'prop-types';
+
 // importing functionalities
-import { createContext, useContext, useState, useEffect } from 'react';
+import { createContext, useContext, useState } from 'react';
 
 // creating context
 const PokemonContext = createContext();
 
 /// Declaring AppProvider
-const AppProvider = ({ children }) => {
+const PokemonProvider = ({ children }) => {
   // states
   // state for all the pokémons
   const [allPokemons, setAllPokemons] = useState([]);
@@ -25,60 +28,6 @@ const AppProvider = ({ children }) => {
   // state for the infinite scroll
   const [message, setMessage] = useState('');
   const [isLoading, setLoading] = useState(true);
-
-  // defining effects
-
-  // effects
-  // using to get the pokemon data
-  // useEffect(getPokemonsWhileScroll, []);
-  useEffect(getPokemonFromSearch, []);
-
-  // fetching the data and updating the states
-  const fetchData = async () => {
-    setLoading(true);
-    setMessage('loading');
-    const response = await getAllPokemons(1);
-    setAllPokemons(response);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  // useEffect to handle the scroll event for the user
-  useEffect(() => {
-    const handleScroll = () => {
-      // verifying if scroll event is fired
-      console.log('event fired!');
-
-      // for testing purpouses, verify if the length is 70
-      if (allPokemons.length > 70) {
-        setMessage('Reached the end of the list!!');
-        return;
-      }
-      if (
-        window.innerHeight + document.documentElement.scrollTop ===
-        document.documentElement.offsetHeight
-      ) {
-        setMessage('Loading...');
-        setLoading(true);
-        // the getPokemonData method is adding one, because it was duplicating the data
-        // the array starts at zero, because of that we need to add one
-        // to prevent mutating the array directly, we use the spread operator
-        getAllPokemons(allPokemons.length + 1).then((newPokemons) => {
-          setAllPokemons([...allPokemons, ...newPokemons]);
-          setLoading(false);
-        });
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [allPokemons]);
 
   // defining state constant
   const state = {
@@ -97,7 +46,7 @@ const AppProvider = ({ children }) => {
   };
 
   // returning passing a child prop to the component
-  return <AppContext.Provider value={state}>{children}</AppContext.Provider>;
+  return <PokemonContext.Provider value={state}>{children}</PokemonContext.Provider>;
 };
 
 // Declaring useAppContext
@@ -105,6 +54,11 @@ const usePokemonContext = () => {
   return useContext(PokemonContext);
 };
 
+// prop types validation
+PokemonProvider.propTypes = {
+  children: PropTypes.node,
+};
+
 // Exporting
-export default AppProvider;
-export { usePokemonContext as useAppContext };
+export default PokemonProvider;
+export { usePokemonContext };
