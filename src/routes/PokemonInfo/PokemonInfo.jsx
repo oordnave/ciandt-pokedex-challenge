ca59dd9eb4a7574853df4c8881a9b87de08c5e55
Evandro Nascimento <oordnave@gmail.com>
2023-10-02 05:11:50 -0300

    Changes files, removes unused components, adds simple id validation from route

--- src/routes/PokemonInfo/PokemonInfo.jsx
+++ src/routes/PokemonInfo/PokemonInfo.jsx
@@ -1,13 +1,19 @@
 // pokemon info component
+// import useParams
+import { useParams } from 'react-router-dom';
 
 // components
 import Header from '../../components/blocks/Header/Header';
 
 const PokemonInfo = () => {
+  let { pokemonId } = useParams();
+
+  console.log('the id from the route is:', { pokemonId });
+
   return (
     <>
       <Header />
-      <div>Pokémon Exists!</div>
+      <div>Pokémon Exists! Id: {pokemonId}</div>
     </>
   );
 };
